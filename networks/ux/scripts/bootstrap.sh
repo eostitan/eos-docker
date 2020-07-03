632304c6b4b977214ccd6b3036f3f6041ce1b4d0
systemzax <gbabint@msn.com>
2020-07-03 12:41:18 -0400

    Updated bootstrap and uxprotocol-deploy files

--- networks/ux/scripts/bootstrap.sh
+++ networks/ux/scripts/bootstrap.sh
@@ -8,6 +8,7 @@ DATADIR="data"
 
 echo $CONTAINER
 
+
 if [ `docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 wallet list |grep $WALLETNAME|wc -l` -lt 1 ]
 then
   echo "Create development wallet"
@@ -54,93 +55,67 @@ docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux
 echo "Create eosio.rex user"
 docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 create account eosio eosio.rex EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
 
-#bash $DIR/uxprotocol-deploy.sh clone build deploy
-#bash $DIR/uxprotocol-deploy.sh build deploy
 bash $DIR/uxprotocol-deploy.sh build deploy
 
+#bash $DIR/uxprotocol-deploy.sh clone
+#bash $DIR/uxprotocol-deploy.sh clone build deploy
+#bash $DIR/uxprotocol-deploy.sh build deploy
+#bash $DIR/uxprotocol-deploy.sh deploy
+
 ########### Initialize testnet chain
+echo ""
 echo "Creating UTX / UTXRAM Tokens"
 docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio.token create '[ "eosio", "2500000000.0000 UTX"]' -p eosio.token@active
-docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio.token create '[ "eosio", "8000000.0000 UTXRAM"]' -p eosio.token@active
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio.token create '[ "eosio", "8388608.0000 UTXRAM"]' -p eosio.token@active
 
+echo ""
 echo "Issuing UTX Token to eosio"
-docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio.token issue '[ "eosio", "1000.0000 UTX", "eosio" ]' -p eosio@active
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio.token issue '[ "eosio", "180000000.0000 UTX", "eosio" ]' -p eosio@active
 
+echo ""
 echo "Init eosio.system Contract"
 docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio init '[0, "4,UTX"]' -p eosio@active
 
-#docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio setparams '{"params":{"max_block_net_usage": 1048576, "target_block_net_usage_pct": 1000, "max_transaction_net_usage": 524288, "base_per_transaction_net_usage": 12, "net_usage_leeway": 500, "context_free_discount_net_usage_num": 20, "context_free_discount_net_usage_den": 100, "max_block_cpu_usage": 1500000, "target_block_cpu_usage_pct": 10, "max_transaction_cpu_usage": 1450000, "min_transaction_cpu_usage": 100, "max_transaction_lifetime": 3600, "deferred_trx_expiration_window": 600, "max_transaction_delay": 3888000, "max_inline_action_size": 4096, "max_inline_action_depth": 4, "max_authority_depth": 6}}' -p eosio@active
+echo ""
+echo "Setting global params"
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio setparams '{"params":{"max_block_net_usage": 1048576, "target_block_net_usage_pct": 1000, "max_transaction_net_usage": 524288, "base_per_transaction_net_usage": 12, "net_usage_leeway": 500, "context_free_discount_net_usage_num": 20, "context_free_discount_net_usage_den": 100, "max_block_cpu_usage": 1500000, "target_block_cpu_usage_pct": 1000, "max_transaction_cpu_usage": 1450000, "min_transaction_cpu_usage": 100, "max_transaction_lifetime": 3600, "deferred_trx_expiration_window": 600, "max_transaction_delay": 3888000, "max_inline_action_size": 4096, "max_inline_action_depth": 6, "max_authority_depth": 6}}' -p eosio@active
 
-
+echo ""
 echo "Creating testuser1 account"
 docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system newaccount eosio testuser1 EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV --stake-net "1.0000 UTX" --stake-cpu "1.0000 UTX" --buy-ram "1.0000 UTX" --transfer
 
-docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio.token transfer '["eosio", "testuser1", "1.0000 UTX",  ""]' -p eosio@active
+echo ""
+echo "Transfering tokens to testuser1"
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio.token transfer '["eosio", "testuser1", "160000000.0000 UTX",  ""]' -p eosio@active
 
-docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio.token transfer '["testuser1", "eosio", "0.0001 UTX",  ""]' -p testuser1@active
+echo ""
+echo "attempt to register without sufficient stake (should fail)"
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio regproducer '["testuser1", "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV", "https://eostitan.com", 124]' -p testuser1@active
 
-docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio.token transfer '["testuser1", "eosio", "0.0001 UTX",  ""]' -p testuser1@active
+echo ""
+echo "stake 10 million tokens"
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio delegatebw '{"from":"testuser1", "receiver":"testuser1", "stake_net_quantity":"5000000.0000 UTX", "stake_cpu_quantity":"5000000.0000 UTX", "transfer":0}' -p testuser1@active
 
-#echo "Creating testuser2 account"
-#docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system newaccount eosio testuser2 EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy --stake-net "1.0000 UTX" --stake-cpu "1.0000 UTX" --buy-ram "4.0000 UTX" --transfer
+echo ""
+echo "register with sufficient stake (should work)"
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio regproducer '["testuser1", "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV", "https://eostitan.com", 124]' -p testuser1@active
 
-# echo "Issuing UTX Token to eosio"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio.token issue '[ "eosio", "10000000.0000 UTX", "m" ]' -p eosio@active
+echo ""
+echo "stake 140 million tokens (required for network activation)" 
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio delegatebw '{"from":"testuser1", "receiver":"testuser1", "stake_net_quantity":"70000000.0000 UTX", "stake_cpu_quantity":"70000000.0000 UTX", "transfer":0}' -p testuser1@active
 
-# echo "Listing eosio balance"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 get currency balance eosio.token eosio
+echo ""
+echo "vote producer"
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio voteproducer '{"voter":"testuser1", "proxy":"", "producers":["testuser1"]}' -p testuser1@active
 
-# echo "Buy RAM for eosio"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system buyram eosio eosio "100.0000 UTX"
+echo ""
+echo "attempt to unstake without unregproducer (should fail)"
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 --verbose push action eosio undelegatebw '{"from":"testuser1", "receiver":"testuser1", "unstake_net_quantity":"75000000.0000 UTX", "unstake_cpu_quantity":"74000000.0000 UTX"}' -p testuser1@active
 
-# echo "Stake UTX for eosio" # We must stake enough of the EOS token to initialize the network
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system delegatebw eosio eosio "90000000.0000 UTX" "90000000.0000 UTX"
+echo ""
+echo "unregister"
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio unregprod '["testuser1"]' -p testuser1@active
 
-########### Setup contact accounts
-
-# echo "Create vpow.token user"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system newaccount eosio vpow.token EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy --stake-net "100.0000 UTX" --stake-cpu "100.0000 UTX" --buy-ram "100.0000 UTX"
-
-# echo "Create ux.exchange user"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system newaccount eosio ux.exchange EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy --stake-net "100.0000 UTX" --stake-cpu "100.0000 UTX" --buy-ram "100.0000 UTX"
-
-# echo "Create ict user"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system newaccount eosio ict EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy --stake-net "100.0000 UTX" --stake-cpu "100.0000 UTX" --buy-ram "100.0000 UTX"
-
-# echo "Set vpow.token eosio.code permission"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set account permission vpow.token active --add-code
-
-# echo "Set ux.exchange eosio.code permission"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set account permission ux.exchange active --add-code
-
-# echo "Set ict eosio.code permission"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set account permission ict active --add-code
-
-########### Setup extra producers
-
-# echo "Creating producer1 account"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system newaccount eosio producer1 EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy --stake-net "100.0000 UTX" --stake-cpu "100.0000 UTX" --buy-ram "100.0000 UTX"
-
-# echo "Give producer1 a balance"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 transfer eosio producer1 "1000.0000 UTX" ""
-
-# echo "Register producer1 as producer"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system regproducer producer1 EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-
-# echo "Vote producer1 as producer"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system voteproducer approve eosio producer1
-
-# echo "Creating producer2 account"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system newaccount eosio producer2 EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy EOS6CRG7tXc9u2ySGqkH69JrwG4yXojkZBVUMLgUnKfM6uJpDUtKy --stake-net "100.0000 UTX" --stake-cpu "100.0000 UTX" --buy-ram "100.0000 UTX"
-
-# echo "Give producer2 a balance"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 transfer eosio producer2 "1000.0000 UTX" ""
-
-# echo "Register producer2 as producer"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system regproducer producer2 EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-
-# echo "Vote producer2 as producer"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 system voteproducer approve eosio producer2
-
-# echo "Creating UTX Token"
-# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action vpow.token create '{"issuer":"satoshighost","maximum_supply":"21000000.0000 UTX"}' -p vpow.token@active
+echo ""
+echo "unstake tokens (should work)"
+docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio undelegatebw '{"from":"testuser1", "receiver":"testuser1", "unstake_net_quantity":"75000000.0000 UTX", "unstake_cpu_quantity":"74000000.0000 UTX"}' -p testuser1@active
--- networks/ux/scripts/uxprotocol-deploy.sh
+++ networks/ux/scripts/uxprotocol-deploy.sh
@@ -6,19 +6,22 @@ for action in $@
 do
 	case $action in
 		clone)
-			echo "Clone ux contracts repository"
-			git clone https://github.com/CryptoMechanics/ux.contracts $DIR/../ux.contracts
-#			git clone git@github.com:CryptoMechanics/ux.contracts $DIR/../ux.contracts
+
+			echo "Clone UX contracts repository"
+			git clone https://github.com/CryptoMechanics/ux.contracts $DIR/../contracts/ux.contracts
+
 			;;
 		build)
-			echo "Build ux system contracts using EOSIO.CDT v1.6.3"
-			docker exec ux-main bash -c "cd /root/eosio.contracts && mkdir -p build && ./build.sh -e /usr/opt/eosio/1.8.9/ -c /eosio.cdt/v1.7.0/usr/"
+
+			echo "Build ux system contracts using EOSIO.CDT v1.7.0"
+			docker exec ux-main bash -c "cd /root/contracts/ux.contracts && mkdir -p build && ./build.sh -e /usr/opt/eosio/1.9./ -c /eosio.cdt/v1.7.0/usr/"
+			
 			;;
 		deploy)
 			docker exec -it ux-main curl -X POST http://127.0.0.1:8888/v1/producer/schedule_protocol_feature_activations -d '{"protocol_features_to_activate": ["0ec7e080177b2c02b278d5088611686b49d739925a92d9bfcacd7fc6b74053bd"]}' | jq
 
 			echo "Deploy OLD eosio.system contract"
-			docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set contract eosio /eosio.contracts.old/build/contracts/eosio.system/ eosio.system.wasm eosio.system.abi -p eosio@active
+			docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set contract eosio /eosio.contracts/build/contracts/eosio.system/ eosio.system.wasm eosio.system.abi -p eosio@active
 
 			# Active EOSIO network features
 			docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio activate '["f0af56d2c5a48d60a4a5b5c903edfb7db3a736a94ed589d0b797df33ff9d3e1d"]' -p eosio # GET_SENDER
@@ -35,14 +38,14 @@ do
 			docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 push action eosio activate '["299dcb6af692324b899b39f16d5a530a33062804e41f09dc97e9f156b4476707"]' -p eosio # WTMSIG_BLOCK_SIGNATURES
 
 			# Deploy blockchain smart contracts
-			echo "Deploy eosio.bios contract"
-			docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set contract eosio /eosio.contracts/build/contracts/eosio.bios/ eosio.bios.wasm eosio.bios.abi -p eosio@active
+			echo "Deploy ux eosio.bios contract"
+			docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set contract eosio /root/contracts/ux.contracts/build/contracts/eosio.bios/ eosio.bios.wasm eosio.bios.abi -p eosio@active
 
-			echo "Deploy NEW eosio.system contract"
-			docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set contract eosio /eosio.contracts/build/contracts/eosio.system/ eosio.system.wasm eosio.system.abi -p eosio@active
+			echo "Deploy ux NEW eosio.system contract"
+			docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set contract eosio /root/contracts/ux.contracts/build/contracts/eosio.system/ eosio.system.wasm eosio.system.abi -p eosio@active
 
-			echo "Deploy eosio.token contract"
-			docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set contract eosio.token /eosio.contracts/build/contracts/eosio.token/ eosio.token.wasm eosio.token.abi -p eosio.token@active
+			echo "Deploy ux eosio.token contract"
+			docker exec -it ux-main cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set contract eosio.token /root/contracts/ux.contracts/build/contracts/eosio.token/ eosio.token.wasm eosio.token.abi -p eosio.token@active
 
 			# echo "Deploy eosio.msig contract"
 			# docker exec -it $CONTAINER cleos --url http://127.0.0.1:8888 --wallet-url http://ux-wallet:8901 set contract eosio.msig /eosio.contracts/build/contracts/eosio.msig/ eosio.msig.wasm eosio.msig.abi -p eosio.msig@active
